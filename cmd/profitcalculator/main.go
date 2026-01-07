38e28d92266535cc992bd933717ab6d68063f990
Jack <109554692+SamuraiJack96@users.noreply.github.com>
2026-01-07 13:16:03 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/profitcalculator/main.go
@@ -0,0 +1,23 @@
+// cmd/profitcalculator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "profitcalculator/internal/profitcalculator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := profitcalculator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
